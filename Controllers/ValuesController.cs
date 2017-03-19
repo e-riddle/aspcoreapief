fc796d51d13712c38e0f34c427edff13ed45b2ec
ERIC RIDDLE <eric@email.net>
2017-03-18 21:50:06 -0400

    xml comments

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -32,18 +32,31 @@ namespace aspnetapp.Controllers
             return "value";
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
         // POST api/values
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
